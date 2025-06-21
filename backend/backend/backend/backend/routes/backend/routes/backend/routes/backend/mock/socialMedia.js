f7188e51a9f6f7efad3bd584f07524cade9b243b
vastav99 <vxstxv243@gmail.com>
2025-06-21 10:53:32 +0530

    Create socialMedia.js

---
+++ backend/backend/backend/backend/routes/backend/routes/backend/routes/backend/mock/socialMedia.js
@@ -0,0 +1,6 @@
+module.exports = function getMockSocialMedia(disasterId) {
+  return [
+    { post: "#floodrelief Need food in NYC", user: "citizen1" },
+    { post: "#earthquake help needed", user: "responderA" }
+  ];
+};
